f6f54dc2855d384ae1113238a3fbdb309de95ca5
gkrause <gkrause@guibson>
2017-08-29 12:06:12 -0300

    transferência dos dados do arquivo para o agenteArquivador PART 2

--- src/cliente/AgenteMedico.java
+++ src/cliente/AgenteMedico.java
@@ -213,6 +213,7 @@ public class AgenteMedico extends javax.swing.JFrame {
         //</editor-fold>
 
         /* Create and display the form */
+        
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new AgenteMedico().setVisible(true);
--- src/server/Persistencia.java
+++ src/server/Persistencia.java
@@ -1,97 +1,98 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package server;
 
-import java.awt.List;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
  * @author gkrause
  */
 public class Persistencia {
+
     private int indice;
 
     public Persistencia() {
         this.indice = 0;
     }
 
-    public void lerArquivo() throws FileNotFoundException {     
-        
-        
-        
+    public void lerArquivo() throws FileNotFoundException {
+
         ArrayList<AgenteArquivador> listArquivador = new ArrayList<>();
-        
+
         try {
             Scanner scanner = new Scanner(new FileReader("dados.txt"))
                     .useDelimiter(" ");
 
             while (scanner.hasNext()) {
-                
+
                 listArquivador.add(new AgenteArquivador());
-                
+                this.indice = listArquivador.size();
+                //////// TA DANDO ERRO ESSA MERDA /////////
+
                 String doenca = scanner.next();
-                if (doenca.equals("resfriado"))
-                {
-                    listArquivador.get(indice).setResfriado(true);
+                switch (doenca) {
+                    case "resfriado":
+                        listArquivador.get(indice).setResfriado(true);
+                        break;
+                    case "dengue":
+                        listArquivador.get(indice).setDengue(true);
+                        break;
+                    case "peneumonia":
+                        listArquivador.get(indice).setPneumonia(true);
+                        break;
+                    case "catapora":
+                        listArquivador.get(indice).setCatapora(true);
+                        break;
+                    default:
+                        break;
                 }
-                else if (doenca.equals("dengue"))
-                {
-                    listArquivador.get(indice).setDengue(true);
-                }
-                else if (doenca.equals("peneumonia"))
-                {
-                    listArquivador.get(indice).setPneumonia(true);
-                }
-                else if (doenca.equals("catapora"))
-                {
-                    listArquivador.get(indice).setCatapora(true);
-                }
-                        
+
                 String dor = scanner.next();
-                if (dor.equals("S"))
-                {
+                if (dor.equals("S")) {
                     listArquivador.get(indice).setDor(true);
                 }
-                
+
                 String febre = scanner.next();
-                if (febre.equals("S"))
-                {
+                if (febre.equals("S")) {
                     listArquivador.get(indice).setFebre(true);
                 }
-                
+
                 String manchas = scanner.next();
-                if (manchas.equals("S"))
-                {
+                if (manchas.equals("S")) {
                     listArquivador.get(indice).setManchas(true);
                 }
-                
+
                 String tosse = scanner.next();
-                if (tosse.equals("S"))
-                {
+                if (tosse.equals("S")) {
                     listArquivador.get(indice).setTosse(true);
                 }
-                
+
                 String cansaco = scanner.next();
-                if (cansaco.equals("S"))
-                {
+                if (cansaco.equals("S")) {
                     listArquivador.get(indice).setCansaco(true);
                 }
-                
-                indice ++;
+
+                indice++;
             }
-            scanner.close();
         } catch (FileNotFoundException fileNotFoundException) {
             System.err.println(" Error opening file. ");
             System.exit(0);
         }
+        System.out.println(listArquivador.get(indice).isCatapora());
+        System.out.println(listArquivador.get(indice).isDengue());
+        System.out.println(listArquivador.get(indice).isPneumonia());
+        System.out.println(listArquivador.get(indice).isResfriado());
+        System.out.println(listArquivador.get(indice).isDor());
+        System.out.println(listArquivador.get(indice).isFebre());
+        System.out.println(listArquivador.get(indice).isManchas());
+        System.out.println(listArquivador.get(indice).isTosse());
+        System.out.println(listArquivador.get(indice).isCansaco());
+    }
+    
+    public static void main(String[] args) throws FileNotFoundException {
+        Persistencia x = new Persistencia();
+        x.lerArquivo();
     }
 }
